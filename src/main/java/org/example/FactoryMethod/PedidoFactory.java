0e48f3bfd4ea652112bfebab5d9c47829df8c047
Joel Henrique <104386844+Joel-Henrique@users.noreply.github.com>
2025-03-11 16:22:50 -0300

    atualiza metodo factory

--- src/main/java/org/example/FactoryMethod/PedidoFactory.java
+++ src/main/java/org/example/FactoryMethod/PedidoFactory.java
@@ -2,8 +2,17 @@ package org.example.FactoryMethod;
 
 public class PedidoFactory {
     public static Pedido criarPedido(String tipo, double valor) {
-        if (tipo.equals("normal")) return new PedidoNormal(valor);
-        else if (tipo.equals("expresso")) return new PedidoExpresso(valor);
-        throw new IllegalArgumentException("Tipo de pedido desconhecido");
+        Class<?> classe;
+        Object objeto;
+        try {
+            classe = Class.forName("org.example.FactoryMethod.Pedido" + tipo.substring(0, 1).toUpperCase() + tipo.substring(1));
+            objeto = classe.getConstructor(double.class).newInstance(valor);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Tipo de pedido desconhecido");
+        }
+        if (!(objeto instanceof Pedido)) {
+            throw new IllegalArgumentException("Tipo de pedido inválido");
+        }
+        return (Pedido) objeto;
     }
 }
